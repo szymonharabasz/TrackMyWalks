5fa0609a3d28cf63ef7ccc7ef871358cc2bd3e8a
Szymon Harabasz <szymon.harabasz@gmail.com>
2022-01-25 00:48:52 +0100

    Initial commit, after Chapter 1

---
+++ TrackMyWalks/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using TrackMyWalks.Pages;
+
+namespace TrackMyWalks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                MainPage = new SplashPage();
+            }
+            else
+            {
+                var navPage = new NavigationPage(new TrackMyWalks.Pages.WalksPage())
+                {
+                    Title = "Track My Walks"
+                };
+                MainPage = navPage;
+
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
